88878ff3584221e48d0caa39f6b247a03b4fedb0
GL513 <gvl.051306@gmail.com>
2021-09-21 19:16:18 -0400

    Update rtmmine.sh

--- rtmmine.sh
+++ rtmmine.sh
@@ -1,1 +1,1 @@
-cd && cd cpuminer-gr && ./cpuminer -a gr -o stratum+tcp://stratum-eu.rplant.xyz:17056 -u [ADDRESS] -p TU
+cd && cd cpuminer-gr && ./cpuminer -a gr -o stratum+tcp://stratum-eu.rplant.xyz:17056 -u [ENTER YOUR ADDRESS HERE] -p TU
