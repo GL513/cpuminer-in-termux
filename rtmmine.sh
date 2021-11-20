fca96839dc325fc2cf6233f40f54499b04ec4831
GL513 <gvl.051306@gmail.com>
2021-11-19 19:09:46 -0500

    Update rtmmine.sh

--- rtmmine.sh
+++ rtmmine.sh
@@ -1,1 +1,1 @@
-cd && cd cpuminer-gr && ./cpuminer -a gr -o stratum+tcp://r-pool.net:3008 -u [ENTER YOUR ADDRESS HERE] -p TU
+cd && cd cpuminer-gr && ./cpuminer -a gr -o stratum+tcp://loudmining.com:9420 -u [ENTER YOUR ADDRESS HERE] -p TU
