b768bef738c896dd260490839ce2577e85c1a8bd
GL513 <gvl.051306@gmail.com>
2021-11-22 11:20:53 -0500

    pool swap
    LOUD pool down, reverting to rpool for default pool

--- rtmmine.sh
+++ rtmmine.sh
@@ -2,4 +2,4 @@ cd
 apt update
 apt upgrade -y
 cd cpuminer-gr
-./cpuminer -a gr -o stratum+tcp://loudmining.com:9420 -u [ENTER YOUR ADDRESS HERE] -p x
+./cpuminer -a gr -o stratum+tcp://r-pool.net:3008 -u [ENTER YOUR ADDRESS HERE] -p x
